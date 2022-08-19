cf406372e85017df06cd5635000ba89cf7e6ffaa
auma2022 <auma.amwayi@student.moringaschool.com>
2022-08-19 13:13:19 +0300

    commit tree 3b37e3f7b2b1ccae0eede2eaa52f5b8565a355d9

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
+
 # Write your code here.
+@katz_deli = ["ebbe",'mac']
+def line(array)
+    if array.length <1
+        puts "The line is currently empty."
+    elsif array.length >= 1  then   
+        puts "The Line is currently:"  
+        return array.each_with_index { |item,index| print" #{index+1}. #{item}" }
+    end    
+end
+
+line(@katz_deli)
